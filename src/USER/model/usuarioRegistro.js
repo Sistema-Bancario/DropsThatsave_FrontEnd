fbacacad261431d40ab324fecdd85471c36af125
Juan Avila <javila-2021047@kinal.edu.gt>
2023-07-19 22:09:05 -0600

    avance

---
+++ src/USER/model/usuarioRegistro.js
@@ -0,0 +1,11 @@
+export const userRegistro = {
+    nombre: "",
+    correo:"",
+    password:"",
+    tatuajes: true,
+    tipoSangre: "",
+    telefono: "",
+    direccion: "",
+    enfermedades: true,
+    imagen:""
+}
